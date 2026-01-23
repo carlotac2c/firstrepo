bca73b7af73d46a50702aa3a8b84a5188b9be6ce
carlotacampos <carlotac2c@gmaik.com>
2026-01-23 09:28:24 +0100

    Initial Commit - Age calculator

---
+++ main.py
@@ -0,0 +1,8 @@
+print("This is my first homework")
+
+birth_year = int(input("Enter your birth year: "))
+current_year = 2026
+
+age = current_year - birth_year
+print(f"You are approximately {age} years old.")
+
